0550fde7bd460d0038a3b741dd64d3eb74788aa5
CloroxEnergyDrink <easonpcvryan@gmail.com>
2018-10-13 14:59:18 +0100

    Update main.py

--- game/main.py
+++ game/main.py
@@ -1,5 +1,4 @@
-import pygame
+# Copyright (C) 2018 3CP Studios.
+print("\nStarting programme...\n\n. . .")
+import pygame,random,numpy
 from time import sleep as delay
-import random
-import numpy
-
